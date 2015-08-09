358ce8ecb6cb8038dbd460ba4e6b9288d70fd2e0
Max <titantbx1989215@gmail.com>
2015-08-10 09:06:22 +1200

    a

--- code/MOPSOCD/result/plotBest.R
+++ code/MOPSOCD/result/plotBest.R
@@ -1,5 +1,5 @@
 plotmpso <- function(number = 1, color = "red", pchtype = 1){
-	read_pso_data <- function(filename){
+	read_mpso_data <- function(filename){
 		data <- read.csv(filename, sep = ',', header = T)
 		n <- nrow(data)
 		return(data[1:n,])
@@ -7,9 +7,9 @@ plotmpso <- function(number = 1, color = "red", pchtype = 1){
 
 
 	filename <- paste("/home/st-james1/tanboxi/588_project/code/MOPSOCD/result/",number, '/', number, '.csv', sep = "")
-	pso_data <- read_pso_data(filename)
+	mpso_data <- read_mpso_data(filename)
 	par(font.axis = 2)
-	plot(pso_data[order(pso_data$latencyF), 1:2], xlim = c(0, 1), ylim = c(0, 1), col = color, xlab = 'cost', ylab = 'latency', pch = pchtype, type = 'b')
+	plot(mpso_data[order(mpso_data$latencyF), 1:2], xlim = c(0, 1), ylim = c(0, 1), col = color, xlab = 'cost', ylab = 'latency', pch = pchtype, type = 'b')
 	#legend("topright","MOPSOCD", col = "red", pch = 1)
 }
 
